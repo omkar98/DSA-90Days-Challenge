97835698d156c13ec40553ec23e9cf8971dfb975
omkar98 <edu.omkar@gmail.com>
2024-12-03 20:41:03 -0600

    Not equivalent to the editorial silution. But covers almost many many testcases.

--- Day09/solution.java
+++ Day09/solution.java
@@ -1,12 +1,57 @@
 package Day09;
 
-public class    solution {
+import java.math.BigInteger;
+
+public class  solution {
     public static void main(String[] args) {
-        String input = "1221";
+        String input = "8888888888";
         System.out.println(nextLargestPalindrome(input, input.length()));
     }
 
-    // Brute of Brute Force approach -> O(n*10^n) time complexity!
+    public static String nextLargestPalindrome(String number, int length) {
+        if(length==1) return String.valueOf(Integer.parseInt(number)+1);
+        if(checkPalindrome(number, length))  {
+            number = new BigInteger(number).add(BigInteger.ONE).toString();
+            length = number.length();
+        }
+        String leftNumber = number.substring(0,(length/2));
+        String rightNumber = number.substring(length%2==0 ? (length/2) : (length/2)+1, length);
+        if(length%2==0) {
+            String reversedLeftString = new StringBuilder(leftNumber).reverse().toString();
+            int reversedLeft = Integer.parseInt(new StringBuilder(leftNumber).reverse().toString());
+            if(reversedLeft > Integer.parseInt(rightNumber)) {
+                return leftNumber + reversedLeftString;
+            } else {
+                leftNumber=String.valueOf(Integer.parseInt(leftNumber)+1);
+                rightNumber = new StringBuilder(leftNumber).reverse().toString();
+                return leftNumber+rightNumber;
+            }
+        } else {
+            String reversedLeftString = new StringBuilder(leftNumber).reverse().toString();
+            int reversedLeft = Integer.parseInt(new StringBuilder(leftNumber).reverse().toString());
+            if(reversedLeft > Integer.parseInt(rightNumber)) {
+                return leftNumber + number.charAt(length/2) + reversedLeftString;
+            } else {
+                leftNumber = String.valueOf(Integer.valueOf(Integer.valueOf(leftNumber + number.charAt(length/2))+1));
+                reversedLeftString = new StringBuilder(leftNumber.substring(0,leftNumber.length()-1)).reverse().toString();
+                return leftNumber+reversedLeftString;
+            }
+        }
+    }
+
+    public static boolean checkPalindrome(String number, int length) {
+        int left = 0;
+        int right = length-1;
+        while(left<right) {
+            if(number.charAt(left)!=number.charAt(right)) return false;
+            left++;right--;
+        }
+        return true;
+    }
+
+
+
+    /*// Brute of Brute Force approach -> O(n*10^n) time complexity!
     public static String nextLargestPalindrome(String number, int length) {
         Long num = Long.valueOf(number);
         Long nextPalindrome = num+1;
@@ -14,17 +59,6 @@ public class    solution {
             nextPalindrome++;
         }
         return nextPalindrome.toString();
-    }
-
-    public static boolean checkPalindrome(String number, int length) {
-        int left = 0;
-        int right = length-1;
-        boolean palindrome = false;
-        while(left<right) {
-            if(number.charAt(left)!=number.charAt(right)) return false;
-            left++;right--;
-        }
-        return true;
-    }
+    }*/
 }
 
