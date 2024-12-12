c39672b44b098c4b232ad8f15b95a53c548ba03e
omkar98 <edu.omkar@gmail.com>
2024-12-11 20:29:51 -0600

    Max water container. Check if isSubsequence.

---
+++ Problem22/solution.java
@@ -0,0 +1,31 @@
+package Problem22;
+
+public class  solution {
+    public static void main(String[] args) {
+        // consider 0 as +ve.
+        int[] arr = {4,3,2,1,4};
+        System.out.println(maxArea(arr));
+
+    }
+
+    public static int maxArea(int[] height) {
+        int size = height.length;
+        int left = 0 ;
+        int right  = size-1;
+        int area = 0;
+        while(left<right) {
+            int newArea = Math.min(height[left], height[right]) * (right-left);
+            area = area>newArea ? area : newArea;
+            if(height[left]>height[right]) right--;
+            else if(height[left]<height[right]) left++;
+            else if(height[left] == height[right]) {
+                if(height[left+1]>height[right-1]) left++;
+                else right--;
+            }
+        }
+        return area;
+    }
+}
+
+
+
