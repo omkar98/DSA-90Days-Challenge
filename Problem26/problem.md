f40b8b33850ba67bef1f3aa6e974b6221dcf430b
omkar98 <edu.omkar@gmail.com>
2024-12-14 22:32:50 -0600

    kadane's algorithm

---
+++ Problem26/problem.md
@@ -0,0 +1,5 @@
+This is a new problem
+```
+<h1>Kadane's Algorithm</h1>
+```
+Kadane's Algorithm. Watch Striver's YT video to understand further.
---
+++ Problem26/solution.java
@@ -0,0 +1,22 @@
+package Problem26;
+
+public class  solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 7, -4, 3, 2, -10, 9, 1};
+        System.out.println(maxSubarraySum(arr, arr.length));
+    }
+
+    public static long maxSubarraySum(int[] arr, int n) {
+        int max = 0;
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum+=arr[i];
+            if(sum>max) max = sum;
+            else if(sum<0) sum=0;
+        }
+        return max;
+    }
+}
+
+
+
