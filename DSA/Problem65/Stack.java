e9ed2265a53926813c7201317c72267c2b10e798
omkar98 <edu.omkar@gmail.com>
2025-01-22 16:00:56 -0600

    Stack Using Arrays

--- DSA/Problem65/Stack.java
+++ DSA/Problem65/Stack.java
@@ -1,24 +1,42 @@
 package DSA.Problem65;
 
 public class Stack {
-    int[] stack = new int[];
+    private int[] stack;
+    private int capacity;
+    private int top = -1;
+
     Stack(int capacity) {
-        int[] stack = new int[capacity];
+        this.stack = new int[capacity];
+        this.capacity = capacity;
     }
+
     public void push(int num) {
-        // Write your code here.
+        if(isFull()!=1) {
+            top++;
+            this.stack[top] = num;
+        }
     }
+
     public int pop() {
-        // Write your code here.
+        if(isEmpty()!=1) {
+            int element = this.stack[top];
+            top--;
+            return element;
+        }
+        return -1;
     }
+
     public int top() {
-        // Write your code here.
+        return top!=-1 ? this.stack[top] : -1;
     }
+
     public int isEmpty() {
-        // Write your code here.
+        if(top<0) return 1;
+        else return 0;
     }
     public int isFull() {
-        // Write your code here.
+        if(top>=capacity-1) return 1;
+        else return 0;
     }
 }
 
