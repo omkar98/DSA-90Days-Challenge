244d4dd8601c14cc5f38a32b2632e6ef9fd30b95
omkar98 <edu.omkar@gmail.com>
2025-05-11 14:22:41 -0500

    unique tree from pre and in order traversals

--- DSA/Problem86/Solution.java
+++ DSA/Problem86/Solution.java
@@ -1,59 +1,64 @@
 package DSA.Problem86;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
-class TreeNode<T> {
-    T data;
-    TreeNode<T> left;
-    TreeNode<T> right;
-
-    public TreeNode(T val) {
-        this.data = val;
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+        this.data = 0;
         this.left = null;
         this.right = null;
     }
-}
-
-
-class QueueElement {
-    TreeNode node;
-    Integer index;
-    Integer width;
-
-    public QueueElement(TreeNode node, Integer index, Integer width) {
-        this.node = node;
-        this.index = index;
-        this.width = width;
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-}
-
-
+    TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+};
 
 public class Solution {
 
     public static void main(String[] args) {
-        TreeNode<Integer> root = new TreeNode<>(1);
-        root.left = new TreeNode<>(2);
-        root.right = new TreeNode<>(3);
-        root.left.left = new TreeNode<>(4);
-        root.left.right = new TreeNode<>(5);
-        root.right.left = new TreeNode<>(6);
-        root.right.right = new TreeNode<>(7);
-        System.out.print(getMaxWidth(root));
+        int[] inorder = {40,20,50,10,60,30};
+        int[] preorder = {10,20,40,50,30,60};
+        TreeNode root = buildBinaryTree(inorder, preorder);
+        System.out.print(root.data);
+        //printTree(root);
     }
 
-    public static int getMaxWidth(TreeNode root) {
-        Queue<QueueElement> queue = new LinkedList<>();
-        if(root==null) return -1;
+    public static TreeNode buildBinaryTree(int[] inorder, int[] preorder) {
+        TreeNode root = new TreeNode(preorder[0]);
+        return buildBinaryTreeAlgo(root, inorder, preorder);
+    }
 
-        queue.add(new QueueElement(root,0, 1));
-        int width = 1;
-
-        while(!queue.isEmpty()) {
-            QueueElement element = queue.remove();
-            // under progress
+    public static TreeNode buildBinaryTreeAlgo(TreeNode root, int[] inorder, int[] preorder) {
+        if(inorder.length==0 && preorder.length==0) {
+            return null;
         }
+        int rootIndex = findIndex(preorder[0],inorder);
+        root = new TreeNode(preorder[0]);
+        TreeNode leftNode =  buildBinaryTreeAlgo(root, Arrays.copyOfRange(inorder, 0,rootIndex), Arrays.copyOfRange(preorder, 1,rootIndex+1));
+        if(leftNode!=null) root.left = leftNode;
+        TreeNode rightNode = buildBinaryTreeAlgo(root, Arrays.copyOfRange(inorder, rootIndex+1,inorder.length), Arrays.copyOfRange(preorder, rootIndex+1,preorder.length));
+        if(rightNode!=null) root.right = rightNode;
+        return root;
+    }
 
+    public static int findIndex (int element, int[] array) {
+       for(int i=0; i<array.length; i++) {
+           if(element==array[i]) return i;
+       }
+       return -1;
+    }
+
+    public static int[] buildBinaryTree(TreeNode root) {
+        return null;
     }
 }
