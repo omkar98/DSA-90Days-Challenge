d6024a4c96e0c1a9f31d73e6b8c176cbb9b5f8f0
omkar98 <edu.omkar@gmail.com>
2025-07-08 07:41:49 -0500

    search in a BST

--- DSA/Problem98/Solution.java
+++ DSA/Problem98/Solution.java
@@ -1,24 +1,22 @@
 package DSA.Problem98;
 
 
-import java.util.ArrayList;
-import java.util.List;
 
-class TreeNode {
+class BinaryTreeNode {
     int data;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+    BinaryTreeNode() {
         this.data = 0;
         this.left = null;
         this.right = null;
     }
-    TreeNode(int data) {
+    BinaryTreeNode(int data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
-    TreeNode(int data, TreeNode left, TreeNode right) {
+    BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right) {
         this.data = data;
         this.left = left;
         this.right = right;
@@ -28,43 +26,33 @@ class TreeNode {
 public class Solution
 {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        node1.left = new TreeNode(2);
-        node1.right = new TreeNode(3);
+        BinaryTreeNode node1 = new BinaryTreeNode(8);
+        node1.left = new BinaryTreeNode(5);
+        node1.right = new BinaryTreeNode(12);
 
-        node1.left.left = new TreeNode(4);
-        node1.left.right = new TreeNode(5);
+        node1.left.left = new BinaryTreeNode(4);
+        node1.left.right = new BinaryTreeNode(7);
 
-//        node1.right.left = new TreeNode(4);
-//        node1.right.right = new TreeNode(5);
+        node1.right.left = new BinaryTreeNode(10);
+        node1.right.right = new BinaryTreeNode(14);
 
-//        node1.left.left.left = new TreeNode<>(7);
-//        node1.left.left.right = new TreeNode<>(8);
+//        node1.left.left.left = new BinaryTreeNode<>(7);
+//        node1.left.left.right = new BinaryTreeNode<>(8);
 
-//        node1.left.right.left = new TreeNode<>(9);
-        node1.left.right.right = new TreeNode(6);
-        TreeNode result = flattenBinaryTree(node1);
+//        node1.left.right.left = new BinaryTreeNode<>(9);
+//        node1.left.right.right = new BinaryTreeNode(6);
+
+        boolean result = searchInBST(node1, 11);
         System.out.print(result);
 
 
     }
 
-    public static TreeNode flattenBinaryTree(TreeNode root) {
-        TreeNode curr = root;
-        while(curr!=null) {
-            if(curr.left!=null) {
-                TreeNode rightNode = curr.left;
-                while(rightNode.right!=null) {
-                    rightNode = rightNode.right;
-                }
-                rightNode.right = curr.right;
-                curr.right = curr.left;
-                curr = curr.left;
-            } else {
-                curr = curr.right;
-            }
+    public static boolean searchInBST(BinaryTreeNode root, int x) {
+        while(root!=null && root.data!=x) {
+            root = root.data<x ? root.right : root.left;
         }
-        return root;
+        return root!=null && root.data==x;
     }
 
 
---
+++ DSA/Problem99/Solution.java
@@ -0,0 +1,68 @@
+package DSA.Problem99;
+
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+        this.data = 0;
+        this.left = null;
+        this.right = null;
+    }
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+};
+
+public class Solution
+{
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        node1.left = new TreeNode(2);
+        node1.right = new TreeNode(3);
+
+        node1.left.left = new TreeNode(4);
+        node1.left.right = new TreeNode(5);
+
+//        node1.right.left = new TreeNode(4);
+//        node1.right.right = new TreeNode(5);
+
+//        node1.left.left.left = new TreeNode<>(7);
+//        node1.left.left.right = new TreeNode<>(8);
+
+//        node1.left.right.left = new TreeNode<>(9);
+        node1.left.right.right = new TreeNode(6);
+        TreeNode result = flattenBinaryTree(node1);
+        System.out.print(result);
+
+
+    }
+
+    public static TreeNode flattenBinaryTree(TreeNode root) {
+        TreeNode curr = root;
+        while(curr!=null) {
+            if(curr.left!=null) {
+                TreeNode rightNode = curr.left;
+                while(rightNode.right!=null) {
+                    rightNode = rightNode.right;
+                }
+                rightNode.right = curr.right;
+                curr.right = curr.left;
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return root;
+    }
+
+
+}
