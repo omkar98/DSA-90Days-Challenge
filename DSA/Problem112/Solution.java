e13fd1b35aac7c33008e5ffb0de0eb3a0963209e
omkar98 <edu.omkar@gmail.com>
2025-07-19 13:33:02 -0500

    greedy cookies

--- DSA/Problem112/Solution.java
+++ DSA/Problem112/Solution.java
@@ -1,122 +1,35 @@
 package DSA.Problem112;
 
+import java.util.Arrays;
 import java.util.Stack;
-
-class TreeNode<T>
-{
-    public T data;
-    public TreeNode<T> left;
-    public TreeNode<T> right;
-
-    TreeNode(T data)
-    {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
 
 public class Solution
 {
-    static Stack<TreeNode> stkAsc = new Stack();
-    static Stack<TreeNode> stkDesc = new Stack();
+
     public static void main(String[] arr){
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode<>(3);
-        root.right = new TreeNode<>(6);
-//        root.right.left = new TreeNode<>(2);
-//        root.right.left.left = new TreeNode<>(8);
-        root.left.left = new TreeNode<>(2);
-        root.left.right = new TreeNode(4);
-        root.right.right = new TreeNode(7);
-//        root.left.right.left = new TreeNode(5);
-//        root.left.right.left.left= new TreeNode(4);
-//        root.left.left.left = new TreeNode<>(1);
-//        BSTIteratorASC iterator = new BSTIteratorASC(root);
-//        while(iterator.hasNext())
-//        {
-//            System.out.println(iterator.next()+" ");
-//        }
-        System.out.print(pairSumBst(root, 14));
+
+        int[] greed = new int[]{3,5,6,4};
+        int[] size = new int[]{10,10};
+        System.out.print(assignCookie(greed, size));
 
     }
 
-    static class BSTIteratorASC{
-
-        BSTIteratorASC(TreeNode<Integer> root){
-            if(root!=null){
-                stkAsc.push(root);
-                while(root.left!=null) {
-                    stkAsc.push(root.left);
-                    root = root.left;
-                }
-            }
-        }
-
-        int next(){
-            TreeNode element = null;
-            if(hasNext()){
-                element = stkAsc.pop();
-                if(element.right!=null){
-                    new BSTIteratorASC(element.right);
-                }
-                return (int)element.data;
+    public static int assignCookie(int []child, int []cookie) {
+        Arrays.sort(child);
+        Arrays.sort(cookie);
+        int cookiePtr = 0;
+        int childPtr = 0;
+        int noOfChildren = child.length;
+        int noOfCookies = cookie.length;
+        while(cookiePtr<noOfCookies && childPtr<noOfChildren){
+            if(cookie[cookiePtr]>=child[childPtr]) {
+                cookiePtr++;
+                childPtr++;
             } else {
-                return -1;
+                cookiePtr++;
             }
         }
-
-        boolean hasNext(){
-            return !stkAsc.isEmpty();
-        }
-    }
-
-    static class BSTIteratorDESC{
-
-        BSTIteratorDESC(TreeNode<Integer> root){
-            if(root!=null){
-                stkDesc.push(root);
-                while(root.right!=null) {
-                    stkDesc.push(root.right);
-                    root = root.right;
-                }
-            }
-        }
-
-        int before(){
-            TreeNode element = null;
-            if(hasBefore()){
-                element = stkDesc.pop();
-                if(element.left!=null){
-                    new BSTIteratorDESC(element.left);
-                }
-                return (int)element.data;
-            } else {
-                return -1;
-            }
-        }
-
-        boolean hasBefore(){
-            return !stkDesc.isEmpty();
-        }
-    }
-
-    public static boolean pairSumBst(TreeNode root, int k) {
-        BSTIteratorASC p1 = new BSTIteratorASC(root);
-        BSTIteratorDESC p2 = new BSTIteratorDESC(root);
-        int val = -1;
-        int p1Val = p1.next();
-        int p2Val = p2.before();
-        while(val!=k && p1Val<p2Val) {
-            val = p1Val+p2Val;
-           if(val>k) {
-               p2Val =  p2.before();
-           } else {
-               p1Val = p1.next();
-           }
-        }
-        return val==k;
-
+        return childPtr;
     }
 
 }
