fd42f43bd0ac592a79bc709eec6dd49065acf157
omkar98 <edu.omkar@gmail.com>
2025-07-21 08:44:44 -0500

    116 - Jump Game. Possible or not possible.

--- DSA/Problem116/Solution.java
+++ DSA/Problem116/Solution.java
@@ -7,24 +7,30 @@ public class Solution
 {
 
     public static void main(String[] arr){
-        int [] jump = new int[]{1,2,3,1,1,0,2,5};
-        System.out.print(jump_search(jump, 8));
+        int [] jump = new int[]{2,1,3,2,4};
+        System.out.print(minimumJumps(jump, 5));
     }
 
-    public static boolean jump_search(int []arr, int n) {
-        boolean isZero = false;
-        for(int i=0; i<n;i++){
-            if(arr[i]==0) isZero = true;
+    public static int minimumJumps(int[] arr, int N) {
+        int leftPtr = 0;
+        int rightPtr = 0;
+        int minJumps = 0;
+        while(rightPtr<N-1){
+            int newRightPtr = rightPtr;
+            for(int i=leftPtr; i<=rightPtr; i++){
+                int currIndex = arr[i]+i;
+                if (currIndex>rightPtr) newRightPtr = currIndex;
+                if (newRightPtr >= N - 1) {
+                    return minJumps + 1;
+                }
+            }
+            if (newRightPtr == rightPtr) {
+                return -1; // If no progress is made, return -1
+            }
+            leftPtr = rightPtr+1;
+            rightPtr= newRightPtr;
+            minJumps+=1;
         }
-        if(!isZero) return true;
-
-        int maxIndex = 0;
-        int currentIndex = 0;
-        int nthPosition = n;
-        while(currentIndex<=maxIndex && maxIndex<nthPosition){
-            maxIndex = Math.max(maxIndex, arr[currentIndex]+currentIndex);
-            currentIndex++;
-        }
-        return maxIndex>=nthPosition;
+        return minJumps;
     }
 }
---
+++ DSA/Problem117/Solution.java
@@ -0,0 +1,28 @@
+package DSA.Problem117;
+
+
+public class Solution
+{
+
+    public static void main(String[] arr){
+        int [] jump = new int[]{1,2,3,1,1,0,2,5};
+        System.out.print(jump_search(jump, 8));
+    }
+
+    public static boolean jump_search(int []arr, int n) {
+        boolean isZero = false;
+        for(int i=0; i<n;i++){
+            if(arr[i]==0) isZero = true;
+        }
+        if(!isZero) return true;
+
+        int maxIndex = 0;
+        int currentIndex = 0;
+        int nthPosition = n;
+        while(currentIndex<=maxIndex && maxIndex<nthPosition){
+            maxIndex = Math.max(maxIndex, arr[currentIndex]+currentIndex);
+            currentIndex++;
+        }
+        return maxIndex>=nthPosition;
+    }
+}
