f99f99c82d0732a779d4ec824c05572678e39ecf
omkar98 <edu.omkar@gmail.com>
2025-07-23 07:55:03 -0500

    120 - Non-overlapping intervals

--- DSA/Problem120/Solution.java
+++ DSA/Problem120/Solution.java
@@ -1,50 +1,30 @@
 package DSA.Problem120;
 
 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-class Interval{
-    int startTime;
-    int endTime;
-    int position;
-
-    Interval(int startTime, int endTime, int position){
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.position = position;
-    }
-}
 public class Solution
 {
 
     public static void main(String[] arr){
-        int[] start = new int[]{0,3,1,5,5,8};
-        int[] end = new int[]{5,4,2,9,7,9};
-        int maxMeetings = maximumMeetings(start, end);
+        int[][] intervals = new int[][]{{0,5}, {3,4}, {1,2}, {5,9}, {5,7}, {7,9}};
+        int maxMeetings = minimumReschedules(intervals.length,intervals);
         System.out.print(maxMeetings);
     }
 
-    public static int maximumMeetings(int []start, int []end) {
-        int noOfMeetings = start.length;
-        List<Interval> intervals = new ArrayList<>();
-        for(int i=0; i<noOfMeetings; i++){
-            intervals.add(new Interval(start[i], end[i], i+1));
-        }
-        intervals.sort(Comparator.comparingInt(interval -> interval.endTime));
-        int freeTime = 0;
+    public static int minimumReschedules(int N, int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[1]));
+        int endTime = -1;
         int count = 0;
-        List<Integer> meetings = new ArrayList<>();
-        for(int i=0; i<noOfMeetings; i++){
-            Interval currentMeeting = intervals.get(i);
-            if(currentMeeting.startTime>freeTime) {
-                freeTime = currentMeeting.endTime;
+        for(int i=0; i<N; i++){
+            if(intervals[i][0]>=endTime) {
                 count++;
-                meetings.add(currentMeeting.position);
+                endTime = intervals[i][1];
             }
         }
-
-        return count;
+        return N-count;
     }
 }
