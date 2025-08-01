8b8e036ee89970f44778dfad33f97342f084f7a6
omkar98 <edu.omkar@gmail.com>
2025-08-01 08:22:46 -0500

    Complete String Solution

--- DSA/Problem128/Solution.java
+++ DSA/Problem128/Solution.java
@@ -1,27 +1,81 @@
 package DSA.Problem128;
 
+import java.util.Arrays;
 
-import java.util.ArrayList;
-import java.util.Objects;
+class Node{
+    Node[] links;
+    boolean end;
+    Node(){
+        this.links = new Node[26];
+        end = false;
+    }
+}
+
+class Trie {
+    Node root;
+    Trie(){
+        this.root = new Node();
+    }
+
+    public void insert(String str){
+        int strLeng = str.length();
+        Node currNode = this.root;
+        for(int i=0; i<strLeng; i++){
+            int letterIndex = str.charAt(i) - 'a';
+            if(currNode.links[letterIndex]==null){
+                Node newNode = new Node();
+                currNode.links[letterIndex] = newNode;
+            }
+            currNode = currNode.links[letterIndex];
+        }
+        currNode.end = true;
+    }
+
+    public boolean commonPrefix(String str){
+        int strLeng = str.length();
+        Node currNode = this.root;
+        for(int i=0; i<strLeng; i++){
+            int letterIndex = str.charAt(i) - 'a';
+            currNode = currNode.links[letterIndex];
+            if(currNode==null || !currNode.end){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 
 public class Solution
 {
-    public static int findDuplicate(ArrayList<Integer> arr, int n){
-        arr.sort((a,b)-> a-b);
-        for(int i=0; i<n-1; i++){
-            if(Objects.equals(arr.get(i), arr.get(i + 1))) return arr.get(i);
+
+    public static String completeString(int n, String[] a) {
+        Trie trie = new Trie();
+        for(int strNo=0; strNo<n;strNo++){
+            String str = a[strNo];
+            trie.insert(str);
         }
-        return -1;
+        String longestPrefixString = "";
+        for(int strNo=0; strNo<n;strNo++){
+            String str = a[strNo];
+            boolean isCommon = trie.commonPrefix(str);
+            if(isCommon && longestPrefixString.length()<=str.length()) {
+                if(longestPrefixString.length()==str.length() && !longestPrefixString.isBlank()){
+                    String[] strArr = new String[2];
+                    strArr[0] = longestPrefixString;
+                    strArr[1] = str;
+                    Arrays.sort(strArr);
+                    longestPrefixString = strArr[0];
+                } else {
+                    longestPrefixString = str;
+                }
+            }
+        }
+        return longestPrefixString.isBlank() ? "None" : longestPrefixString;
     }
 
     public static void main(String[] arr){
-        ArrayList<Integer> arr1  = new ArrayList<>();
-        arr1.add(4);
-        arr1.add(1);
-        arr1.add(6);
-        arr1.add(1);
-        System.out.print(findDuplicate(arr1, arr1.size()));
-
-
+        String[] a = new String[]{ "g" ,"l", "lm", "ga", "lmn", "gaz"};
+        System.out.print(completeString(a.length, a));
     }
 }
---
+++ DSA/Problem129/Solution.java
@@ -0,0 +1,27 @@
+package DSA.Problem129;
+
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Solution
+{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        arr.sort((a,b)-> a-b);
+        for(int i=0; i<n-1; i++){
+            if(Objects.equals(arr.get(i), arr.get(i + 1))) return arr.get(i);
+        }
+        return -1;
+    }
+
+    public static void main(String[] arr){
+        ArrayList<Integer> arr1  = new ArrayList<>();
+        arr1.add(4);
+        arr1.add(1);
+        arr1.add(6);
+        arr1.add(1);
+        System.out.print(findDuplicate(arr1, arr1.size()));
+
+
+    }
+}
