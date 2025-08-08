f7ee283c56883898b360d480b59f4a3541eb2d40
omkar98 <edu.omkar@gmail.com>
2025-08-08 08:11:18 -0500

    Longest SubString with Atmost K chars -- Optimal Codewise.

--- DSA/TwoPointerSlidingWindow/Problem137/Solution.java
+++ DSA/TwoPointerSlidingWindow/Problem137/Solution.java
@@ -14,20 +14,17 @@ public class Solution
         int left=0; int right=0; int maxLen = 0; int strLen = str.length();
         Map<Character, Integer> mpp = new HashMap<>();
         while(right<strLen) {
-            if(mpp.size()<=k) {
-                char c = str.charAt(right);
-                int value = mpp.getOrDefault(c, 0);
-                mpp.put(c,value+1);
-                if(mpp.size()<=k) maxLen = Math.max(maxLen, right-left+1);
-                right++;
-            } else {
-                while(mpp.size()>k){
-                    char c = str.charAt(left);
-                    mpp.computeIfPresent(c, (key, value) -> value-1);
-                    if(mpp.get(c) == 0) mpp.remove(c);
-                    left++;
-                }
+            char c = str.charAt(right);
+            int value = mpp.getOrDefault(c, 0);
+            mpp.put(c,value+1);
+            while(mpp.size()>k){
+                char c1 = str.charAt(left);
+                mpp.computeIfPresent(c1, (key, val) -> val-1);
+                if(mpp.get(c1) == 0) mpp.remove(c1);
+                left++;
             }
+            maxLen = Math.max(maxLen, right-left+1);
+            right++;
         }
         return maxLen;
     }
