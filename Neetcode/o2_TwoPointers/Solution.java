41b6b6633d112bd9942f9cc405608eb28960ec6b
omkar98 <edu.omkar@gmail.com>
2026-01-01 16:57:58 -0600

    Solved in less than 5 mins, but after watching the solution.

--- Neetcode/o2_TwoPointers/Solution.java
+++ Neetcode/o2_TwoPointers/Solution.java
@@ -4,5 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         ValidPalindrome.isPalindrome("Was it a car or a cat I saw?");
         TwoSum.twoSum(new int[]{1,3,4,5,7,10,11}, 9);
+        ThreeSum.threeSum(new int[]{-1,0,1,2,-1,-4,-2,-3,3,0,4});
     }
 }
